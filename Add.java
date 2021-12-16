e5fb6dca0fd104e15f4a9f8a7b56cc98638af9d7
mennab <mennaw11@gmail.com>
2021-12-16 15:22:58 +0200

    added

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 class Name {
     public static void main(String[] args) {
         int x =40;
-        int y =10;
+        int y =50;
         int z = x + y;
         System.out.println("the value is " + z); 
     }
